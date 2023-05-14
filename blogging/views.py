dd893a8edf88f4489c9c97c3d6c1e1d257e2a157
CAAAR12 <crisr714@gmail.com>
2023-05-14 16:18:25 -0700

    replace view with render shortcut and passed

--- blogging/views.py
+++ blogging/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.template import loader
 from blogging.models import Post
+from django.shortcuts import render
 
 def stub_view(request, *args, **kwargs):
     body = "Stub View\n\n"
@@ -16,7 +17,5 @@ def stub_view(request, *args, **kwargs):
 def list_view(request):
     published = Post.objects.exclude(published_date__exact=None)
     posts = published.order_by('-published_date')
-    template = loader.get_template('blogging/list.html')
     context = {'posts': posts}
-    body = template.render(context)
-    return HttpResponse(body, content_type="text/html")
+    return render(request,'blogging/list.html', context)
