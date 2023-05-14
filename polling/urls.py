f3d2383e8e1ca09efbd020b61036d32003ba3e88
CAAAR12 <crisr714@gmail.com>
2023-05-14 12:25:31 -0700

    created polling templates

--- polling/urls.py
+++ polling/urls.py
@@ -13,9 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+from polling.views import list_view, detail_view
 from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', list_view, name="poll_index"),
+    path('polls/<int:poll_id>/', detail_view, name="poll_detail"),
 ]
