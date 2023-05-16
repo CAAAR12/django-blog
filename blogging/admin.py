47afa96c4f4def808f6f81c8e406b29bbb86e1de
CAAAR12 <crisr714@gmail.com>
2023-05-15 20:50:43 -0700

    figured out inline for categories

--- blogging/admin.py
+++ blogging/admin.py
@@ -1,6 +1,22 @@
 from django.contrib import admin
 from blogging.models import Post,Category
 
+
+class CategoryInLine(admin.StackedInline):
+    model = Category.posts.through
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title',
+                    'text',
+                    'author',
+                    'created_date','modified_date','published_date')
+    inlines = [CategoryInLine]
+
+class CategoryAdmin(admin.ModelAdmin):
+    inlines = [CategoryInLine]
+    exclude = ('Posts',)
+
+
 # Register your models here.
-admin.site.register(Post)
+admin.site.register(Post,PostAdmin)
 admin.site.register(Category)
